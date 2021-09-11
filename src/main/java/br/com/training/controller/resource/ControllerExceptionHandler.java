f3fbd4a6518a6a1a9a2f347a83fa98b0918d9f4e
Gleydsonilo Negre <gleydsontaichou@gmail.com>
2021-09-11 17:37:58 -0300

    Add Service Layer resolve #2

---
+++ src/main/java/br/com/training/controller/resource/ControllerExceptionHandler.java
@@ -0,0 +1,19 @@
+package br.com.training.controller.resource;
+
+import br.com.training.service.exception.DataIntegrityException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(DataIntegrityException.class)
+    public ResponseEntity<StandardError> dataIntegrity (DataIntegrityException e, HttpServletRequest request){
+        StandardError standardError = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(standardError);
+    }
+}
